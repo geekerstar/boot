ef8f9dd0d79c4945ee984878e9f9f6700a121c54
geekerstar <247507792@qq.com>
2023-02-28 19:05:19 +0800

    starter

---
+++ boot-function/src/test/java/com/geekerstar/function/starter/StarterTest.java
@@ -0,0 +1,21 @@
+package com.geekerstar.function.starter;
+
+import com.geekerstar.IdGenerateService;
+import com.geekerstar.function.FunctionApplicationTests;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author geekerstar
+ * @date 2023/2/28 18:57
+ */
+public class StarterTest extends FunctionApplicationTests {
+
+    @Autowired
+    private IdGenerateService idGenerateService;
+
+    @Test
+    public void test1(){
+        System.out.println(idGenerateService.generate());
+    }
+}
